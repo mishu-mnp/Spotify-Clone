04a03b60f66848d3bd04de871735e98d37092ac2
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-11-29 02:07:23 +0530

    footer in process

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './Footer.css';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+
 
 function Footer() {
     return (
         <div className='footer'>
-            <div className="footer_left">
+            <div className="footer__left">
                 <p>Album and song details</p>
             </div>
 
             <div className="footer__center">
-                <p>Player Controls</p>
+                <ShuffleIcon className='footer__green' />
+                <SkipPreviousIcon className='footer__icon' />
+                <PlayCircleOutlineIcon fontSize='large' className='footer__icon' />
+                <SkipNextIcon className='footer__icon' />
+                <RepeatIcon className='footer__green' />
             </div>
 
             <div className="footer__right">

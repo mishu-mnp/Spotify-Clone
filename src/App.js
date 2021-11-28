bfb2eeb28ebdc6fd4e5549fbceef5a8646c48df9
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-11-28 15:33:39 +0530

    working on Data Layer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
 import './App.css';
 import Login from './Login';
 import Player from './Player';
 import { getTokenFromUrl } from './spotify';
 
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -12,18 +14,24 @@ function App() {
   useEffect(() => {
 
     const hash = getTokenFromUrl();
-    // console.log("HASH >>> ", hash);
+    console.log("HASH >>> ", hash);
 
     // if we don't wanna to see token in URL
     window.location.hash = "";
 
     const _token = hash.access_token;
-    console.log("I HAVE A TOKEN >>> ", _token)
 
     if (_token) {
       setToken(_token)
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log("SHOW ME USER", user);
+      })
     }
 
+    console.log("I HAVE A TOKEN >>> ", _token)
   }, []);
 
 

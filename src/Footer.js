b53b76c42fbd29d31566e960ab4745aa74f105b5
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-11-29 11:36:01 +0530

    Footer completed

--- src/Footer.js
+++ src/Footer.js
@@ -5,13 +5,21 @@ import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import ShuffleIcon from '@material-ui/icons/Shuffle';
 import RepeatIcon from '@material-ui/icons/Repeat';
+import { Grid, Slider } from '@material-ui/core';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import images from './components/images';
 
 
 function Footer() {
     return (
         <div className='footer'>
             <div className="footer__left">
-                <p>Album and song details</p>
+                <img className='footer__albumLogo' src={images.spotifyLogo} alt="" />
+                <div className="footer__songInfo">
+                    <h4>Yeah!!!</h4>
+                    <p>User</p>
+                </div>
             </div>
 
             <div className="footer__center">
@@ -23,7 +31,17 @@ function Footer() {
             </div>
 
             <div className="footer__right">
-                <p>Volume Controls</p>
+                <Grid container spacing={2} >
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
             </div>
         </div>
     )
